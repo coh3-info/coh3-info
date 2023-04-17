9a755d40e96a2605cc0ce82a09c88636fe7740e1
jihun <jihunv8@gmail.com>
2023-04-17 16:15:47 +0900

    Feat Header에 Beta label, 게임 버전 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,7 +6,14 @@ const Header = () => {
       <HeaderInner>
         <Title>
           COH3 <span>INFO</span>
+          <BetaLabel>.BETA</BetaLabel>
         </Title>
+        <div>
+          <GameDataVersion>
+            GameData <br />
+            v1.1.3
+          </GameDataVersion>
+        </div>
       </HeaderInner>
     </HeaderWrapper>
   );
@@ -23,15 +30,33 @@ const HeaderInner = styled.div`
   margin: 0 auto;
   padding: 20px 0;
   height: 75px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Title = styled.h1`
   color: #ffffff;
-  font-size: 28px;
+  font-size: 1.75rem;
   font-weight: 300;
 
   > span {
-    font-size: 24px;
+    font-size: 1.375rem;
     font-weight: 100;
   }
+`;
+
+const GameDataVersion = styled.div`
+  font-size: 13px;
+  font-weight: 100;
+  text-align: center;
+  color: #ffffff;
+`;
+
+const BetaLabel = styled.div`
+  display: inline-block;
+  font-size: 0.875rem;
+  color: #ffffff;
+  font-weight: 300;
+  margin-left: 3px;
 `;
